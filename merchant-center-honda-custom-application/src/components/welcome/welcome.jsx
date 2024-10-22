597367ab6de3d632b82c9dcb9744aadd6f844b9e
Jabar141 <97104195+Jabar141@users.noreply.github.com>
2024-10-22 22:29:28 +0530

    Update welcome.jsx

--- merchant-center-honda-custom-application/src/components/welcome/welcome.jsx
+++ merchant-center-honda-custom-application/src/components/welcome/welcome.jsx
@@ -1,12 +1,4 @@
-import {
-  usePaginationState,
-  useDataTableSortingState,
-} from '@commercetools-uikit/hooks';
-import { ContentNotification } from '@commercetools-uikit/notifications';
-import Text from '@commercetools-uikit/text';
 
-import { useProductFetcher } from '../../hooks/use-product-connector';
-import { getErrorMessage } from '../../helpers';
 const Welcome = () => {
 return "hello World"
 }
