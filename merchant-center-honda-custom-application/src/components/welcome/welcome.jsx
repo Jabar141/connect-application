cad3b3a38e88533b1d4021bbaf6ddd6e25b98f13
Jabar141 <97104195+Jabar141@users.noreply.github.com>
2024-10-22 21:47:03 +0530

    Update welcome.jsx

--- merchant-center-honda-custom-application/src/components/welcome/welcome.jsx
+++ merchant-center-honda-custom-application/src/components/welcome/welcome.jsx
@@ -7,8 +7,9 @@ import Text from '@commercetools-uikit/text';
 
 import { useProductFetcher } from '../../hooks/use-product-connector';
 import { getErrorMessage } from '../../helpers';
-
+const Welcome = () => {
 return "hello World"
+}
 Welcome.displayName = 'Home';
 
 export default Welcome;
