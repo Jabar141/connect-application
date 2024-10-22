c4e587ae9c0b823c53503efe392ddaca066ecf3c
Jabar141 <97104195+Jabar141@users.noreply.github.com>
2024-10-22 21:30:31 +0530

    Update welcome.jsx

--- merchant-center-honda-custom-application/src/components/welcome/welcome.jsx
+++ merchant-center-honda-custom-application/src/components/welcome/welcome.jsx
@@ -8,36 +8,7 @@ import Text from '@commercetools-uikit/text';
 import { useProductFetcher } from '../../hooks/use-product-connector';
 import { getErrorMessage } from '../../helpers';
 
-const Welcome = () => {
-  const { page, perPage } = usePaginationState();
-  const tableSorting = useDataTableSortingState({ key: 'key', order: 'asc' });
-  const { productsResult, error, loading } = useProductFetcher({
-    page,
-    perPage,
-    tableSorting,
-  });
-
-  if (error) {
-    return (
-      <ContentNotification type="error">
-        <Text.Body>{getErrorMessage(error)}</Text.Body>
-      </ContentNotification>
-    );
-  }
-
-  if (loading) {
-    return <>Loading...</>;
-  }
-
-  return productsResult.products.results.map((product) => {
-    return (
-      <div key={product.id}>
-        <h1>{product.key}</h1>
-        <p>{product.id}</p>
-      </div>
-    );
-  });
-};
+return "hello World"
 Welcome.displayName = 'Home';
 
 export default Welcome;
